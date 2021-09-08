da6e69b04654ac79a4b2e0b438fec2b64ba4280f
TgJe <51323906+tangjie133@users.noreply.github.com>
2021-09-08 17:05:09 +0800

    更新 pxt.json, main.blocks, NecIR.cpp

--- NecIR.cpp
+++ NecIR.cpp
@@ -12,10 +12,11 @@
 #endif
 #endif
 
-
 #if MICROBIT_CODAL
 
 #else // MICROBIT_CODAL
+
+
 
 class PullMode
 {
@@ -41,6 +42,7 @@ public:
 namespace maqueenIRV2 { 
 int ir_code = 0x00;
 int ir_addr = 0x00;
+int data1 = 0;
 int data;
 
 int logic_value(){//判断逻辑值"0"和"1"子函数
@@ -48,12 +50,14 @@ int logic_value(){//判断逻辑值"0"和"1"子函数
     uint32_t nowtime;
     while(!uBit.io.P16.getDigitalValue());//低等待
     nowtime = system_timer_current_time_us();
-    if((nowtime - lasttime) > 400 && (nowtime - lasttime) < 700){//低电平560us
+    //uBit.serial.printf("3 %d\r\n",(nowtime - lasttime));
+    if((nowtime - lasttime) > 460 && (nowtime - lasttime) < 660){//低电平560us
         while(uBit.io.P16.getDigitalValue());//是高就等待
         lasttime = system_timer_current_time_us();
-        if((lasttime - nowtime)>400 && (lasttime - nowtime) < 700){//接着高电平560us
+        //uBit.serial.printf("%d\r\n",(lasttime - nowtime));
+        if((lasttime - nowtime)>460 && (lasttime - nowtime) < 660){//接着高电平560us
             return 0;
-        }else if((lasttime - nowtime)>1400 && (lasttime - nowtime) < 1800){//接着高电平1.7ms
+        }else if((lasttime - nowtime)>1600 && (lasttime - nowtime) < 1800){//接着高电平1.7ms
             return 1;
        }
     }
@@ -101,21 +105,28 @@ void remote_decode(void){
     while(!uBit.io.P16.getDigitalValue());//低等待
     //uBit.serial.printf("3\r\n");
     nowtime = system_timer_current_time_us();
-    if((nowtime - lasttime) < 10000 && (nowtime - lasttime) > 8000){//9ms
+    //uBit.serial.printf("1 %d\r\n",(nowtime - lasttime));
+    if((nowtime - lasttime) < 9100 && (nowtime - lasttime) > 8800){//9ms
         while(uBit.io.P16.getDigitalValue());//高等待
         lasttime = system_timer_current_time_us();
-        if((lasttime - nowtime) > 4000 && (lasttime - nowtime) < 5000){//4.5ms,接收到了红外协议头且是新发送的数据。开始解析逻辑0和1
+        //uBit.serial.printf("1 %d\r\n",(lasttime - nowtime));
+        if((lasttime - nowtime) > 4400 && (lasttime - nowtime) < 4510){//4.5ms,接收到了红外协议头且是新发送的数据。开始解析逻辑0和1
             pulse_deal();
             //uBit.serial.printf("addr=0x%X,code = 0x%X\r\n",ir_addr,ir_code);
-            data = ir_code;
-            return;//ir_code;
-        }else if((lasttime - nowtime) > 1750 && (lasttime - nowtime) < 2750){//2.25ms,表示发的跟上一个包一致
+            //uBit.serial.printf("1\r\n");
+            data = data1 = ir_code;
+            
+            //return;//ir_code;
+        }else if((lasttime - nowtime) > 2150 && (lasttime - nowtime) < 2260){//2.25ms,表示发的跟上一个包一致
+        
             while(!uBit.io.P16.getDigitalValue());//低等待
             nowtime = system_timer_current_time_us();
-            if((nowtime - lasttime) > 400 && (nowtime - lasttime) < 700){//560us
+            if((nowtime - lasttime) > 460 && (nowtime - lasttime) < 660){//560us
                 //uBit.serial.printf("addr=0x%X,code = 0x%X\r\n",ir_addr,ir_code);
-                data = ir_code;
-                return;//ir_code;
+                //uBit.serial.printf("2\r\n");
+                data = data1;
+                
+                //return;//ir_code;
             }
         }
     }
